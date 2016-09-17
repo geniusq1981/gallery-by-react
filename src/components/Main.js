46f2340e48294427ef73cb156d219a63cba43130
elang <geniusq1981@hotmail.com>
2016-09-17 18:03:52 +0800

    917 final

--- src/components/Main.js
+++ src/components/Main.js
@@ -89,7 +89,37 @@ class ImgFigure extends React.Component {
 		)	
 	}	
 }
+//控制组件 
+class ControllerUnit extends React.Component {
+	constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e){
+		
+		e.stopPropagation();
+		e.preventDefault();
+	}
+	
+	
+	render(){
+		 let controlelrUnitClassName = "controller-unit";
 
+	        // 如果对应的是居中的图片，显示控制按钮的居中态
+	       /* if (this.props.arrange.isCenter) {
+	            controlelrUnitClassName += " is-center";
+
+	            // 如果同时对应的是翻转图片， 显示控制按钮的翻转态
+	            if (this.props.arrange.isInverse) {
+	                controlelrUnitClassName += " is-inverse";
+	            }
+	        }*/
+		return(
+		<span className={controlelrUnitClassName} onClick={this.handleClick}></span>		
+		);
+	}
+	
+}
 class AppComponent extends React.Component {
 	constructor(props) {
         super(props);
@@ -268,6 +298,7 @@ class AppComponent extends React.Component {
 			 }
 		 }
 		 imgFigures.push(<ImgFigure data={value} ref={"imgFigure" + index} arrange={this.state.imgArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
+		 controllerUnits.push(<ControllerUnit/>);
 	 }.bind(this));
     return (
      <section className="stage" ref="stage">
